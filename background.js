eee717d7523b1bfed0b42d4519efd9dc99bcdc7a
YCJ <ycj@Macintosh.local>
2019-06-24 17:55:35 +0800

    first commit

---
+++ background.js
@@ -0,0 +1,28 @@
+
+setInterval(checkState, 100);
+var laststate = ""
+var oldWindowsState = ""
+const homeurl = "http://www.google.com"	// URL of the new tab
+
+function checkState() {
+  waitTime = 300;	// Duration of idle state : 300sec = 5min
+
+  chrome.idle.queryState(waitTime, function(state) {
+    if (laststate != state) {
+			laststate = state;
+			if (state == "idle") {
+				chrome.tabs.create({ url: homeurl })
+				chrome.windows.getCurrent(null, function(window) {
+					oldWindowsState = window.state;
+					chrome.windows.update(window.id, { state: "fullscreen" });
+				})
+			}
+			else if (state == "active") {
+				chrome.windows.getCurrent(null, function(window) {
+					if (oldWindowsState != "")
+						chrome.windows.update(window.id, { state: oldWindowsState });
+				})
+			}
+    }
+  });
+};
