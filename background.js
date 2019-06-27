6b54f90efe974394380b85f7b0b8668397251b90
YCJ <ycj@Macintosh.local>
2019-06-27 09:06:33 +0800

    import json

--- background.js
+++ background.js
@@ -2,7 +2,8 @@
 setInterval(checkState, 100);
 var laststate = ""
 var oldWindowsState = ""
-const homeurl = "https://pixelthoughts.co"	// URL of the new tab
+var homeurl = "https://pixelthoughts.co"	// URL of the new tab
+var waitTime = 15;
 
 function checkState() {
   waitTime = 15;	// Duration of idle state : 300sec = 5min
@@ -29,17 +30,17 @@ function checkState() {
     }
   });
 };
-/*
+
 chrome.runtime.getPackageDirectoryEntry(function(root) {
 	root.getFile("1.json", {}, function(fileEntry) {
 		fileEntry.file(function(file) {
 			var reader = new FileReader();
 			reader.onloadend = function(e) {
 				var myFile = JSON.parse(this.result);
-				alert(myFile["1"])
+				homeurl = myFile["posts"][0]["url"];
+				waitTime = myFile["posts"][0]["idle"];
 			};
 			reader.readAsText(file);
 		});
 	});
 });
-*/
