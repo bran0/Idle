2b051bd134ac70d0c523e73e7813bd08761465a4
YCJ <ycj@Macintosh.local>
2019-06-27 11:23:27 +0800

    fix

--- background.js
+++ background.js
@@ -4,20 +4,20 @@ var oldWindowsState = "";
 var homeurl = "";	// URL of the new tab
 var waitTime = 0;
 
-chrome.runtime.getPackageDirectoryEntry(function(root) {
-	root.getFile("1.json", {}, function(fileEntry) {
-		fileEntry.file(function(file) {
-			var reader = new FileReader();
-			reader.onloadend = function(e) {
-				var myFile = JSON.parse(this.result);
-				homeurl = myFile["posts"][0]["url"];
-				waitTime = parseInt(myFile["posts"][0]["idle"]);
-				setInterval(checkState, 1000);
-			};
-			reader.readAsText(file);
+function loadData() {
+	chrome.runtime.getPackageDirectoryEntry(function(root) {
+		root.getFile("1.json", {}, function(fileEntry) {
+			fileEntry.file(function(file) {
+				var reader = new FileReader();
+				reader.onloadend = function(e) {
+					var myFile = JSON.parse(this.result);
+					homeurl = myFile["posts"][0]["url"];
+					waitTime = parseInt(myFile["posts"][0]["idle"]);
+				};
+			});
 		});
 	});
-});
+}
 
 function checkState() {
 	if (waitTime  <= 0)
@@ -27,6 +27,7 @@ function checkState() {
 		if (laststate != state) {
 			laststate = state;
 			if (state == "idle") {
+				loadData();
 				chrome.tabs.getSelected(null, function(tab) {
 					if (tab.url.indexOf("screendynamics.com") < 0)
 						chrome.tabs.create({ url: homeurl })
@@ -48,4 +49,5 @@ function checkState() {
 	});
 };
 
+loadData();
 setInterval(checkState, 1000);
